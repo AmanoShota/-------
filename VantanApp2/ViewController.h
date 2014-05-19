35e2ecd19a5b3be649b926bc5ecfbcd266e08c30
AmanoShota <amanoshota15@gmail.com>
2014-05-19 14:47:17 +0900

    Initial Commit

---
+++ VantanApp2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VantanApp2
+//
+//  Created by AmanoShota on 2014/05/19.
+//  Copyright (c) 2014年 ShotaAmano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
